c3c05a5f08bb986bb975f9ff2a6886f375bc1849
dharshi756 <112377845+dharshi756@users.noreply.github.com>
2022-08-29 13:01:19 +0530

    Add files via upload

---
+++ dharshi1.py
@@ -0,0 +1,16 @@
+a=int(input("enter the value of a"))
+b=int(input("enter the value of b"))
+c=(a+b)
+print(c)
+d=(a-b)
+print(d)
+e=(a*b)
+print(e)
+f=(a/b)
+print(f)
+g=(a**b)
+print(g)
+      
+      
+
+           
