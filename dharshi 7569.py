5fc32597af03361ff95ca4d8e2fe049e1f7c9b47
dharshi756 <112377845+dharshi756@users.noreply.github.com>
2022-09-26 11:15:28 +0530

    Add files via upload

---
+++ dharshi 7569.py
@@ -0,0 +1,10 @@
+reg=int(input("enter the no.of loaves purchased"))
+old=int(input("enter the no.of old bread purchased"))
+e=185
+r=185*reg
+a=r+dif(reg<0 or old<0);
+print("invaild input")
+print("regular price",e)
+print("amount of purchase loavers",r)
+print("amount of purchase old bread",d)
+print("total amount".a)
